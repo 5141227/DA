3e7508889c39d5733648a6889a6e50d166e29182
5141227 <alopezmacias@willamette.edu>
2025-07-10 18:45:12 +0000

    d

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function hello() {
     alert("Hello, world!")
     
 } /* Enclose the action in {} */
+
+function hello() {
+    alert("Hello, world!")
+}
+
+document.addEventListener('click', hello);
+/* "document" refers the website, 
+   "click" to what you do, 
+   "hello" to what JavaScript does. */
+
+   function hello(event) {
+    alert(event)
+}
+
+document.addEventListener('click', hello(event));
