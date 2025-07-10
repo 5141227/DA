e931ea3d3a24afece73cdca190d797c896f21dc0
5141227 <alopezmacias@willamette.edu>
2025-07-10 19:10:58 +0000

    d

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ function hello(event) {
   element.style["left"] = event.pageX + "px"; /* x is given in "px" */
   alert(event.pageX + "px"); /* So we can see it */
 }
+
+<div style="background:green;" onclick="hello()">
+  <div id="name">
+      Text
+  </div>
+</div>
+
+function hello(event) {
+    alert(3 * event.pageX / window.innerWidth)
+}
+
+function hello(event) {
+    /* Find the element by id */
+    const element = document.getElementById("pupil");
+    /* Find the new "left" value*/
+    const new = 3 * event.pageX / window.innerWidth;
+    /* Update */
+    element.style["left"] = new + "vmin";
+}
+
+document.addEventListener('click', hello(event));
+
+function hello(event) {
+    /* Find the element by id */
+    const element = document.getElementById("pupil");
+    /* Find the new "x" value*/
+    const x = 3 * event.pageX / window.innerWidth;
+    /* Find the new "y" value*/
+    const y = 3 * event.pageY / window.innerHeight;
+    /* Update */
+    element.style["left"] = x + "vmin";
+    element.style["top"] = y + "vmin";
+}
+
+document.addEventListener('click', hello(event));
+
